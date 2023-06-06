8c7f622d83d7d1716060df9dea0a7320b7334ec6
Adrian0429 <royankaruna@gmail.com>
2023-06-06 14:35:03 +0700

    fix relationship

--- entities/jenis_ruangan.go
+++ entities/jenis_ruangan.go
@@ -1,11 +1,6 @@
 package entities
 
-import (
-
-)
-
-
 type Jenis_Ruangan struct {
-	ID_Jenis_Ruangan int `gorm:"type:int;primaryKey;not null" json:"id_jenis_ruangan"`
-	Jenis_Ruangan string `gorm:"type:varchar(30);not null" json:"jenis_ruangan"`
+	ID_Jenis_Ruangan string `gorm:"type:int;primaryKey;not null" json:"id_jenis_ruangan"`
+	Jenis_Ruangan    string `gorm:"type:varchar(30);not null" json:"jenis_ruangan"`
 }
