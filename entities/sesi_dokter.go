e1feaa0fedc2519c36d262118e7b3330024b52a2
Adrian0429 <royankaruna@gmail.com>
2023-06-07 17:11:57 +0700

    rusak lagi ges

--- entities/sesi_dokter.go
+++ entities/sesi_dokter.go
@@ -7,12 +7,12 @@ type Sesi_Dokter struct {
 	Jam_Keluar         int    `gorm:"type:int;;not null" json:"jam_keluar"`
 	Maks_Jumlah_Pasien int    `gorm:"type:int;;not null" json:"maks_Pasien"`
 
-	RuanganID string  `gorm:"type:varchar(5)" json:"ruangan_id" binding:"required"`
-	Ruangan   Ruangan `gorm:"foreignKey:RuanganID" json:"ruangan"`
+	// RuanganID string  `gorm:"type:varchar(5)" json:"ruangan_id" binding:"required"`
+	Ruangan Ruangan `gorm:"foreignKey:RuanganID" json:"ruangan"`
 
-	PerawatID string  `gorm:"type:varchar(4)" json:"id_perawat" binding:"required"`
-	Perawat   Perawat `gorm:"foreignKey:PerawatID" json:"perawat"`
+	// PerawatID string  `gorm:"type:varchar(4)" json:"id_perawat" binding:"required"`
+	Perawat Perawat `gorm:"foreignKey:PerawatID" json:"perawat"`
 
-	Dokter_ID_Dokter string `gorm:"type:char(8)" json:"dokter_id_dokter"`
-	Dokter           Dokter `gorm:"foreignKey:Dokter_ID_Dokter" json:"dokter"`
+	// Dokter_ID_Dokter string `gorm:"type:char(8)" json:"dokter_id_dokter"`
+	Dokter Dokter `gorm:"foreignKey:Dokter_ID_Dokter" json:"dokter"`
 }
